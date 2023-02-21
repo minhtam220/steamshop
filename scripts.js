25b26fe262538b503403006738e2c6c4dd05ef01
minhtam220 <minhtam220@gmail.com>
2023-02-21 10:09:03 +0700

    build 4
    - implement initial load. -> done

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-//MAIN CODES
+//NEW CODES
 //declare variables
 const BASE_URL = `https://steam-api-dot-cs-platform-306304.et.r.appspot.com`;
+
+//variables for select by genre
 let genresList;
 let genreSelected = "";
 
+//variables for select by tag
 let tagsList;
 let tagSelected = "";
 
@@ -12,55 +15,14 @@ let search = "";
 let gamesList;
 let gameSelected;
 
-//initialize the page on first load
-const init = async () => {
-  let genres = [];
-  let tags = [];
+let genres = [];
+let tags = [];
 
-  try {
+let currentPage = 1;
+const limit = 10;
+let total = 0;
 
-    // Get genres list from the API
-    genres = await getAllGenres();
-    // ' len is zero
-    if (!genres.length) {
-      console.log("No genres.");
-      return;
-    }
-    
-    let selectGenresList = document.getElementById("select_genre");
-   
-    genres.forEach((genre, index) => {
-      //create option for each genre 
-      let option = document.createElement("option");
-      option.value = option.text = `${genre.name}`;
-      selectGenresList.add(option);
-    });
-
-
-    // Get tags list from the API
-    tags = await getAllTags();
-    // ' len is zero
-    if (!tags.length) {
-      console.log("No tags.");
-      return;
-    }
-
-    let selectTagsList = document.getElementById("select_tag");
-        
-    tags.forEach((tag, index) => {
-      //create option for each tag
-      let option = document.createElement("option");
-      option.value = option.text = `${tag.name}`;
-      selectTagsList.add(option);
-    });
-
-    renderGames();
-
-  } catch (err) {
-    console.log("err", err);
-  }
-};
-
+//COMMON FUNCTIONS
 //fetch data from the URL
 async function fetchData(url) {
 
@@ -69,10 +31,7 @@ async function fetchData(url) {
   try {
     const response = await fetch(`${url}`);
     if (response.ok) {
-      const data = await response.json();
-      const result = data["data"];
-      console.log(result);
-      return result;
+      return await response.json();
     }
   } catch (error) {
     console.log(error);
@@ -81,45 +40,277 @@ async function fetchData(url) {
 
 }
 
-//get the genres list
-async function getAllGenres() {
+//PROCEDURAL FUNCTIONS
+//initialize the page on first load
+const init = async () => {
 
-  let GENRES_URL = BASE_URL + `/genres/?`;
+  try {
+
+    //calling loadGenres to load genres
+    loadGenres();
+
+    //calling loadGenres to load tags
+    loadTags();
+
+    //calling loadGames to load games based on current page index
+    loadGames(currentPage, limit);
+
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+
+// init() => loadGenres()
+// load genres
+const loadGenres = async () => {
+
+  // show the loader
+  //showLoader();
+
+  // 0.5 second later
+  setTimeout(async () => {
+      try {
+
+        console.log("loading Genres");
+
+        // call the API to get games
+        const response = await getGenres();
+
+        // show games
+        showGenres(response.data);
+
+      } catch (error) {
+          console.log(error.message);
+      } finally {
+
+        console.log("loaded Genres");
+
+        //hideLoader();
+
+      }
+  }, 500);
+
+};
+
+// loadGenres(page,limit) => getGenres(page,limit)
+// get genres 
+const getGenres = async() => {
+
+  let url = BASE_URL + `/genres/?`;
   
   const searchParam = new URLSearchParams(`?`);
 
-  let genres = [];
+  searchParam.append("page",1);
 
-  let data = [];
+  searchParam.append("limit",1000);
+
+  const response = await fetchData(url + searchParam.toString());
+
+  return response;
+
+}
+
+let selectGenresList = document.getElementById("select_genre");
+
+// loadGames(page,limit) => showGenres(games)
+// render the select for genres
+const showGenres = (genres) => {
+
+  genres.forEach((genre, index) => {
+    //create option for each genre 
+    let option = document.createElement("option");
+    option.value = option.text = `${genre.name}`;
+    selectGenresList.add(option);
+  });
   
-  let pageIndex = 1;
+};
 
-  const dataLimit = 100;
 
-  let dataLength = 101;
+// init() => loadTags()
+// load tags
+const loadTags = async () => {
 
-  while (pageIndex < 2 ) {
+  // show the loader
+  //showLoader();
 
-    searchParam.append("page",pageIndex);
+  // 0.5 second later
+  setTimeout(async () => {
+      try {
 
-    searchParam.append("limit",dataLimit);
+        console.log("loading Tags");
 
-    data = await fetchData(GENRES_URL + searchParam.toString());
+        // call the API to get games
+        const response = await getTags();
 
-    dataLength = data.length;
+        // show games
+        showTags(response.data);
 
-    genres = genres.concat(data);
+      } catch (error) {
+          console.log(error.message);
+      } finally {
 
-    pageIndex = pageIndex + 1;
+        console.log("loaded Tags");
 
-    searchParam.delete("page");
+        //hideLoader();
 
-    searchParam.delete("limit");
+      }
+  }, 500);
 
+};
+
+// loadTags(page,limit) => getTags()
+// get tags
+const getTags = async() => {
+
+  let url = BASE_URL + `/steamspy-tags/?`;
+  
+  const searchParam = new URLSearchParams(`?`);
+
+  searchParam.append("page",1);
+
+  searchParam.append("limit",1000);
+
+  const response = await fetchData(url + searchParam.toString());
+
+  return response;
+
+}
+
+let selectTagsList = document.getElementById("select_tag");
+
+// loadTags(page,limit) => showTags(tags)
+// render the select for tags
+const showTags = (tags) => {
+
+  tags.forEach((tag, index) => {
+    //create option for each genre 
+    let option = document.createElement("option");
+    option.value = option.text = `${tag.name}`;
+    selectTagsList.add(option);
+  });
+  
+};
+
+// init() => loadGames(currentPage, limit)
+// load games based on current page index
+const loadGames = async (page, limit) => {
+
+  // show the loader
+  //showLoader();
+
+  console.log("show loader");
+
+  // 0.5 second later
+  setTimeout(async () => {
+      try {
+          // if having more quotes to fetch
+          if (hasMoreGames(page, limit, total)) {
+
+              // call the API to get games
+              const response = await getGames(page, limit);
+
+              // show games
+              showGames(response.data);
+
+              // update the total
+              total = response.total;
+
+              console.log(total);
+
+          }
+      } catch (error) {
+          console.log(error.message);
+      } finally {
+          //hideLoader();
+      }
+  }, 500);
+
+};
+
+// loadGames(page,limit) => hasMoreGames(page, limit, total)
+// check if there are still more games to be loaded
+const hasMoreGames = (page, limit, total) => {
+  const startIndex = (page - 1) * limit + 1;
+  return total === 0 || startIndex < total;
+};
+
+// loadGames(page,limit) => getGames(page,limit)
+// get games based on current page index, 
+// genre
+// tag
+// keywords
+const getGames = async(page,limit) => {
+
+  let url = BASE_URL + `/games/?`;
+  
+  const searchParam = new URLSearchParams(`?`);
+
+  if (search !== ""){
+    searchParam.append("q",search);
+  } 
+  
+  if (genreSelected !== ""){
+    searchParam.append("genres",genreSelected);
+  } 
+
+  if (tagSelected !== ""){
+    searchParam.append("steamspy_tags",tagSelected);
   }
 
-  return genres;
+  searchParam.append("page",page);
+
+  searchParam.append("limit",limit);
+
+  const response = await fetchData(url + searchParam.toString());
+
+  return response;
+
 }
+
+let allGamesList = document.getElementById("games_list");
+
+// loadGames(page,limit) => showGames(games)
+// render the div for games
+const showGames = (games) => {
+
+  let viewURL =  window.location.href.toString();
+
+  viewURL = viewURL.replace("index.html","view.html");
+
+  games.forEach((game, index) => {
+    //Create new `Game Wrapper` for each element
+    let divGameWrapper = document.createElement("div");
+    divGameWrapper.className = "game_wrapper";
+    divGameWrapper.innerHTML = ` 
+        <a href="${viewURL}?appid=${game.appid}" target="_blank"><img
+        src="${game.header_image}"/></a>
+        <div class="game_info">
+          <div class="game_name">${game.name}</div>
+          <div class="game_price">$ ${game.price}</div>
+        </div>
+        `;
+
+    allGamesList.appendChild(divGameWrapper);
+  });
+  
+};
+
+//select genre
+function selectGenre () {
+  
+  let e = document.getElementById("select_genre");
+  genreSelected = e.options[e.selectedIndex].text;
+
+  currentPage = 1;
+  total = 0;
+
+  loadGames(currentPage, limit);
+
+};
+
+
+//OLD CODES
+
 
 //get the tags list
 async function getAllTags() {
@@ -160,17 +351,6 @@ async function getAllTags() {
 
   return tags;
 }
-
-
-//select genre
-function selectGenre () {
-  
-  let e = document.getElementById("select_genre");
-  genreSelected = e.options[e.selectedIndex].text;
-
-  renderGames();
-  
-};
 
 //select tag
 function selectTag () {
